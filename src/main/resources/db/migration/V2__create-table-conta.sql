c649a27fa7685b47164d8ffbe08f3c26a2d7af44
Lucas Moreira <lggm18@hotmail.com>
2025-01-27 17:34:17 -0300

    raw: adicionado novas migrations

--- src/main/resources/db/migration/V2__create-table-conta.sql
+++ src/main/resources/db/migration/V2__create-table-conta.sql
@@ -1,9 +1,9 @@
-CREATE TABLE conta_bancaria (
+CREATE TABLE contas_bancarias (
         id BIGINT AUTO_INCREMENT PRIMARY KEY,
         nome VARCHAR(255) NOT NULL,
         cpf VARCHAR(11) UNIQUE NOT NULL,
         agencia INT NOT NULL DEFAULT 1,
-        conta VARCHAR(255) UNIQUE NOT NULL,
+        numero_conta VARCHAR(255) UNIQUE NOT NULL,
         saldo DOUBLE NOT NULL DEFAULT 0,
         usuario_id BIGINT,
         FOREIGN KEY (usuario_id) REFERENCES usuarios(id)
--- src/main/resources/db/migration/V3__create-table-transacao.sql
+++ src/main/resources/db/migration/V3__create-table-transacao.sql
@@ -1,9 +1,11 @@
-CREATE TABLE transacao (
+CREATE TABLE transacoes (
            id BIGINT AUTO_INCREMENT PRIMARY KEY,
            conta_bancaria_id BIGINT,
+           conta_bancaria_alvo_id BIGINT,
+            tipo_conta varchar(30),
            valor DOUBLE NOT NULL,
            tipo_transacao VARCHAR(255) NOT NULL,
            metodo_transacao VARCHAR(255) NOT NULL,
            data TIMESTAMP NOT NULL,
-           FOREIGN KEY (conta_bancaria_id) REFERENCES conta_bancaria(id)
+           FOREIGN KEY (conta_bancaria_id) REFERENCES contas_bancarias(id)
 );
