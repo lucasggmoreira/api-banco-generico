36162e53fede51a0dafa5abe6d8fc8a04ed989fd
Laboratório FIAP <labsfiap@fiap.com.br>
2025-02-10 21:44:36 -0300

    feat: adicionado endpoint get para retorno de contas bancarias

--- src/main/java/me/lucasggmoreira/banco/controllers/AdminController.java
+++ src/main/java/me/lucasggmoreira/banco/controllers/AdminController.java
@@ -1,6 +1,13 @@
 package me.lucasggmoreira.banco.controllers;
 
 
+import me.lucasggmoreira.banco.domain.contabancaria.ContaBancariaRepository;
+import me.lucasggmoreira.banco.domain.contabancaria.DadosDetalheConta;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,9 +16,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin")
 public class AdminController {
 
-    @GetMapping
-    public String admin(){
-        return "Admin";
+    @Autowired
+    private ContaBancariaRepository contaBancariaRepository;
+
+    @GetMapping("/contas")
+    public ResponseEntity<Page<DadosDetalheConta>> retornarContasBancarias(@PageableDefault(size = 10, sort = "id")Pageable paginacao){
+        var contas = contaBancariaRepository.findAll(paginacao).map(DadosDetalheConta::new);
+        return ResponseEntity.ok(contas);
     }
+
+
 
 }
