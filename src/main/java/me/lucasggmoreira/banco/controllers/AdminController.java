161697c9adff1a2ceb3b0811dd3ebbe6e01bd9b5
Lucas Gabriel Gianini Moreira <lggm18@hotmail.com>
2025-02-11 22:24:56 -0300

    feat: adicionado novo endpoint para retornar usuarios com login

--- src/main/java/me/lucasggmoreira/banco/controllers/AdminController.java
+++ src/main/java/me/lucasggmoreira/banco/controllers/AdminController.java
@@ -3,6 +3,8 @@ package me.lucasggmoreira.banco.controllers;
 
 import me.lucasggmoreira.banco.domain.contabancaria.ContaBancariaRepository;
 import me.lucasggmoreira.banco.domain.contabancaria.DadosDetalheConta;
+import me.lucasggmoreira.banco.domain.usuario.dto.DadosDetalheUsuario;
+import me.lucasggmoreira.banco.domain.usuario.UsuarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,10 +21,19 @@ public class AdminController {
     @Autowired
     private ContaBancariaRepository contaBancariaRepository;
 
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @GetMapping("/contas")
-    public ResponseEntity<Page<DadosDetalheConta>> retornarContasBancarias(@PageableDefault(size = 10, sort = "id")Pageable paginacao){
+    public ResponseEntity<Page<DadosDetalheConta>> retornarContasBancarias(@PageableDefault(size = 15, sort = "id")Pageable paginacao){
         var contas = contaBancariaRepository.findAll(paginacao).map(DadosDetalheConta::new);
         return ResponseEntity.ok(contas);
+    }
+
+    @GetMapping("/usuarios")
+    public ResponseEntity<Page<DadosDetalheUsuario>> retornarUsuarios(@PageableDefault(size = 15, sort = "id")Pageable paginacao){
+        var usuarios = usuarioRepository.findAll(paginacao).map(DadosDetalheUsuario::new);
+        return ResponseEntity.ok(usuarios);
     }
 
 
