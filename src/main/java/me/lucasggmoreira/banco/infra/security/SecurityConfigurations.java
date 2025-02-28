49166018a90b9c85bd2bd5cc07bbc150c199eec1
Lucas Moreiram <lggm18@hotmail.com>
2025-02-28 20:48:24 -0300

    fix: correção da permissão ADMIN

--- src/main/java/me/lucasggmoreira/banco/infra/security/SecurityConfigurations.java
+++ src/main/java/me/lucasggmoreira/banco/infra/security/SecurityConfigurations.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -24,11 +25,11 @@ public class SecurityConfigurations{
                 .authorizeHttpRequests(req -> {
                     req.requestMatchers("/h2-console/**").permitAll();
                     req.requestMatchers("/v3/api-docs/**", "/swagger-ui.html", "/swagger-ui/**").permitAll();
-                    req.requestMatchers("/admin/**").hasAuthority("ROLE_ADMIN");
+                    req.requestMatchers("/admin/**").hasAnyRole("ADMIN");
                     req.requestMatchers("/auth/**").permitAll();
                     req.anyRequest().authenticated();
                 })
-                .headers(headers -> headers.frameOptions(frameOptions -> frameOptions.sameOrigin()))
+                .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
                 .addFilterBefore(securityFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
